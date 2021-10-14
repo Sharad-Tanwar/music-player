3c10ad45b4dfb2e45e3083a8cc73da485ffeec3f
Sharad-Tanwar <80272209+Sharad-Tanwar@users.noreply.github.com>
2021-10-14 20:26:45 +0530

    JavaScript(Complete)-Complete Project

--- data.js
+++ data.js
@@ -1,50 +1,50 @@
 let songs = [
     {
-        name: 'song 1',
+        name: 'Apocalypse',
         path: 'assets/musics/Song 1.mp3',
-        artist: 'artist 1',
+        artist: 'Midranger',
         cover: 'assets/images/cover 1.png'
     },
     {
-        name: 'song 2',
+        name: 'On & On',
         path: 'assets/musics/Song 2.mp3',
-        artist: 'artist 2',
+        artist: 'Cartoon Feat. Danial Levi',
         cover: 'assets/images/cover 2.png'
     },
     {
-        name: 'song 3',
+        name: 'Hand In Glove',
         path: 'assets/musics/Song 3.mp3',
-        artist: 'artist 3',
+        artist: 'Anc D',
         cover: 'assets/images/cover 3.png'
     },
     {
-        name: 'song 4',
+        name: 'The Dazzled Moon',
         path: 'assets/musics/Song 4.mp3',
-        artist: 'artist 4',
+        artist: 'Lone Lover',
         cover: 'assets/images/cover 4.png'
     },
     {
-        name: 'song 5',
+        name: 'Like Me',
         path: 'assets/musics/Song 5.mp3',
-        artist: 'artist 5',
+        artist: 'T-Mass & Britt Lari',
         cover: 'assets/images/cover 5.png'
     },
     {
-        name: 'song 6',
+        name: 'So Happy',
         path: 'assets/musics/Song 6.mp3',
-        artist: 'artist 6',
+        artist: 'Briskit',
         cover: 'assets/images/cover 6.png'
     },
     {
-        name: 'song 7',
+        name: 'Mortals',
         path: 'assets/musics/Song 7.mp3',
-        artist: 'artist 7',
+        artist: 'Warriyo Feat. Laira Brehm',
         cover: 'assets/images/cover 7.png'
     },
     {
-        name: 'song 8',
+        name: 'Safe & Sound',
         path: 'assets/musics/Song 8.mp3',
-        artist: 'artist 8',
+        artist: 'Deaf Kev & Sendi Hoxha',
         cover: 'assets/images/cover 8.png'
     },
 ]
